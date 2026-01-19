10a802518020b39818da80b5bf64b44adf72e4f2
Aaron Singh <aaronhacks10@gmail.com>
2026-01-19 18:39:20 +0530

    implement database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.mongooseCache;
+
+if(!cached){
+
+    cached= global.mongooseCache = { conn: null, promise: null };
+}
